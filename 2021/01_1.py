9c238e6f4af78b487d74d36e0110b052f453cd1e
phumipatc <phumipat.c@outlook.com>
2021-12-04 12:37:19 +0700

    Init

---
+++ 2021/01_1.py
@@ -0,0 +1,18 @@
+"""
+    Task	: 01_1
+    Author	: Phumipat C. [MAGCARI]
+    Language: Python
+    Created	: 04 December 2021 [12:29]
+    Algo	: 
+    Status	: 
+"""
+import numpy as np
+
+with open('01_1.in', 'r') as f:
+    num = [int(x) for x in f.read().split()]
+
+num = np.array(num, dtype=int)
+dif = np.diff(num)
+sign = np.sign(dif)
+
+print(np.sum(sign == 1))
